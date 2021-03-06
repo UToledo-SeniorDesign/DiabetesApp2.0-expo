3e9c8d35c6e8ee6b414a3bcbbbdd1f52a7edbb68
Carlos Galo <cgalo13@gmail.com>
2021-03-05 21:46:54 -0500

    Setup barebone UserProfile screen

---
+++ src/screens/UserProfile.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import type { IUser } from '../types';
+
+const UserProfile:React.FC<{}> = () => {
+    return (
+        <div className="user-page">
+            <h1>User Page!</h1>
+        </div>
+    );
+}
+
+export default UserProfile;
