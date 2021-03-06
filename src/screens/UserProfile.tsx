b3aebaa4002d46c815c43feabbcc7dc8f732e585
Carlos Galo <cgalo13@gmail.com>
2021-03-06 00:38:37 -0500

    Completed basic structure of UserProfile.tsx - Displays the given/prop user information - Updated IUser inside types.ts to have an image attribute

--- src/screens/UserProfile.tsx
+++ src/screens/UserProfile.tsx
@@ -1,13 +1,153 @@
 import React from 'react';
+import { View, SafeAreaView, StyleSheet } from 'react-native';
+import {
+    Avatar,
+    Title,
+    Caption,
+    Text,
+    TouchableRipple,
+} from 'react-native-paper';
+
 
 import type { IUser } from '../types';
 
-const UserProfile:React.FC<{}> = () => {
+
+interface UserProfileProp {
+    user: IUser;
+}
+
+const UserProfile: React.FC<UserProfileProp> = (prop) => {
+    const {
+        first_name,
+        last_name,
+        email,
+        phone,
+        img
+    } = prop.user;
+    const name = first_name + " " + last_name;
+
     return (
-        <div className="user-page">
-            <h1>User Page!</h1>
-        </div>
+        <SafeAreaView style={styles.container}>
+
+            <View style={styles.userInfoSection}>
+                <View style={{ flexDirection: 'row', marginTop: 15 }}>
+                    <Avatar.Image
+                        source={{ uri: img }}
+                        size={80}
+                    />
+                    <View style={{ marginLeft: 20 }}>
+                        <Title style={[styles.title, {
+                            marginTop: 15,
+                            marginBottom: 5,
+                        }]}>{name}</Title>
+                        <Caption style={styles.caption}>{name}</Caption>
+                    </View>
+                </View>
+            </View>
+
+
+            <View style={styles.userInfoSection}>
+                <View style={styles.row}>
+                    <Avatar.Icon size={24} icon="phone" />
+                    <Text style={{ color: "#777777", marginLeft: 20 }}>{phone}</Text>
+                </View>
+                <View style={styles.row}>
+                    <Avatar.Icon size={24} icon="email" />
+                    <Text style={{ color: "#777777", marginLeft: 20 }}>{email}</Text>
+                </View>
+            </View>
+
+            <View style={styles.infoBoxWrapper}>
+                <View style={[styles.infoBox, {
+                    borderRightColor: '#dddddd',
+                    borderRightWidth: 1
+                }]}>
+                    <Title>140</Title>
+                    <Caption>Today's Carbs</Caption>
+                </View>
+                <View style={styles.infoBox}>
+                    <Title>12</Title>
+                    <Caption>Today's Insulin Intake</Caption>
+                </View>
+            </View>
+
+            <View style={styles.menuWrapper}>
+                <TouchableRipple onPress={() => { }}>
+                    <View style={styles.menuItem}>
+                        {/* <Icon name="heart-outline" color="#FF6347" size={25} /> */}
+                        <Text style={styles.menuItemText}>My Meals</Text>
+                    </View>
+                </TouchableRipple>
+                
+                <TouchableRipple onPress={() => { }}>
+                    <View style={styles.menuItem}>
+                        {/* <Icon name="account-check-outline" color="#FF6347" size={25} /> */}
+                        <Text style={styles.menuItemText}>Change Password</Text>    
+                        {/* Note: Maybe leave change password to be handles inside 'Settings' */}
+                    </View>
+                </TouchableRipple>
+
+                <TouchableRipple onPress={() => { }}>
+                    <View style={styles.menuItem}>
+                        {/* <Icon name="settings-outline" color="#FF6347" size={25} /> */}
+                        <Text style={styles.menuItemText}>Settings</Text>
+                    </View>
+                </TouchableRipple>
+            </View>
+
+        </SafeAreaView>
     );
 }
 
 export default UserProfile;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    userInfoSection: {
+        paddingHorizontal: 30,
+        marginBottom: 25,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    caption: {
+        fontSize: 14,
+        lineHeight: 14,
+        fontWeight: '500',
+    },
+    row: {
+        flexDirection: 'row',
+        marginBottom: 10,
+    },
+    infoBoxWrapper: {
+        borderBottomColor: '#dddddd',
+        borderBottomWidth: 1,
+        borderTopColor: '#dddddd',
+        borderTopWidth: 1,
+        flexDirection: 'row',
+        height: 100,
+    },
+    infoBox: {
+        width: '50%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    menuWrapper: {
+        marginTop: 10,
+    },
+    menuItem: {
+        flexDirection: 'row',
+        paddingVertical: 15,
+        paddingHorizontal: 30,
+    },
+    menuItemText: {
+        color: '#777777',
+        marginLeft: 20,
+        fontWeight: '600',
+        fontSize: 16,
+        lineHeight: 26,
+    },
+});
--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,11 @@
 // Shared interfaces
 
+import { Image } from "react-native";
+
 export interface IUser {
     first_name: string;
     last_name:  string;
     email:      string;
     phone:      string;
+    img:        string;
 }
