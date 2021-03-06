3e214fd6ea804c98d3a4439a69e811641f33cb1c
notasimcard <asimtut@gmail.com>
2021-03-06 16:28:48 -0500

    Added routing and screens to route

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const HomeScreen = () => {
+	return (
+		<View style={styles.screen}>
+			<Text>Home section</Text>
+		</View>
+	);
+};
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+});
+
+export default HomeScreen;
