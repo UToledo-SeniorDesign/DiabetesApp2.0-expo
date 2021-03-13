24c8e093811b4273ce0ee51c2b59bbc5dd421846
Carlos Galo <cgalo13@gmail.com>
2021-03-13 16:46:42 -0500

    Updated Input component - Added logic and styling to display error message if the input is invalid

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, memo } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 import { TextInput } from 'react-native-paper';
 
 interface InputProp {
@@ -7,11 +8,13 @@ interface InputProp {
     value?: string;                     // Value of the input
     placeholder?: string;               // Placeholder for the input
     error?: boolean;                    // Whether to style the input with error style
+    errorMsg?: string | undefined;      // Error message to display if the input has an error/invalid
     disabled?: boolean;                 // If true, user won't be able to interact with the component
     mode?: 'flat' | 'outlined' | undefined;  // Flat or outlined input, defauled to flat from react native papers
+    isSensitive?: boolean;              // If the input contains sensitive data, aka password -> Defaulted to false
 }
 
-const MyComponent:React.FC<InputProp> = (prop) => {
+const Input:React.FC<InputProp> = (prop) => {
     const {
         label, 
         value, 
@@ -19,6 +22,8 @@ const MyComponent:React.FC<InputProp> = (prop) => {
         error,
         disabled,
         mode,
+        isSensitive,
+        errorMsg,
         onInput
     } = prop;
 
@@ -30,16 +35,37 @@ const MyComponent:React.FC<InputProp> = (prop) => {
   }
 
   return (
-    <TextInput
-      label={label}
-      value={text}
-      placeholder={placeholder}
-      onChangeText={text => onChange(text)}
-      disabled={disabled}
-      error={error}
-      mode={mode}
-    />
+        <View style={styles.container}>
+            <TextInput
+                label={label}
+                value={text}
+                placeholder={placeholder}
+                onChangeText={text => onChange(text)}
+                disabled={disabled}
+                error={error}
+                mode={mode}
+                secureTextEntry={isSensitive || false}
+            />
+            {error && errorMsg && 
+                <Text style={styles.error}>
+                    {errorMsg}
+                </Text>
+            }
+        </View>
+    
   );
 };
 
-export default MyComponent;
+const styles = StyleSheet.create({
+    container: {
+      width: '100%',
+      marginVertical: 12,
+    },
+    error: {
+      fontSize: 14,
+      paddingHorizontal: 4,
+      paddingTop: 4,
+    },
+  });
+
+export default memo(Input);
