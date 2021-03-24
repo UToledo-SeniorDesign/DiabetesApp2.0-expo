617dae925afd391416678decb00fc0e2afc2d635
notasimcard <asimtut@gmail.com>
2021-03-24 19:56:12 -0400

    cleaned up navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import RootNavigation from "./navigation/RootNavigation";
+import Navigation from "./Navigation";
 
 export default function App() {
-	return (
-		<NavigationContainer>
-			<RootNavigation />
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
+  );
 }
